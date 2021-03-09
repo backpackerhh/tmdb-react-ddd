0c3566d5ad91d20b97baee85c52a5c3de2005dae
David Montesdeoca <backpackerhh@gmail.com>
2021-03-09 22:27:00 +0100

    Allow to go back to previous location from movie details

--- src/Components/GoBack.js
+++ src/Components/GoBack.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const GoBack = () => {
+  const location = useLocation();
+
   return (
     <p>
-      <Link to="/">Go Back</Link>
+      <Link to={location.state.from}>Go Back</Link>
     </p>
   );
 };
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Movie = ({ movie, position }) => {
   const { id, title, voteAverage, releaseDate, poster } = movie;
   const { url, alt, height, title: posterTitle } = poster;
+  const location = useLocation();
+  const path = { pathname: `/movies/${id}`, state: { from: location } };
 
   return (
     <div>
       <strong>#{position}</strong>
 
-      <Link to={`/movies/${id}`}>
+      <Link to={path}>
         <img src={url} alt={alt} height={height} title={posterTitle} />
 
         <div>
