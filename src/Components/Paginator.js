4054e3110e75b8f86c2d66497040dad9aa8adec4
David Montesdeoca <backpackerhh@gmail.com>
2021-03-23 23:18:43 +0100

    Add styling to the paginator

--- src/Components/Paginator.js
+++ src/Components/Paginator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { addQueryParamToCurrentPath } from "../utils/addQueryParamToCurrentPath";
+import "../styles/Paginator.css";
 
 export const Paginator = ({ paginationData }) => {
   const { previousPageNumber, currentPageNumber, nextPageNumber, totalPages } = paginationData;
@@ -9,20 +10,40 @@ export const Paginator = ({ paginationData }) => {
   if (totalPages === 0 || currentPageNumber > totalPages) return null;
 
   return (
-    <p>
-      {previousPageNumber && (
-        <Link to={(location) => addQueryParamToCurrentPath(location, "page", previousPageNumber)}>
-          &#60;&#60; Previous
-        </Link>
-      )}
+    <div className="paginator">
+      <div className="paginator-item first-page">
+        {previousPageNumber && previousPageNumber !== 1 && (
+          <Link to={(location) => addQueryParamToCurrentPath(location, "page", 1)}>« First</Link>
+        )}
+      </div>
 
-      <strong>{currentPageNumber}</strong>
+      <div className="paginator-item previous-page">
+        {previousPageNumber && (
+          <Link to={(location) => addQueryParamToCurrentPath(location, "page", previousPageNumber)}>
+            « Previous
+          </Link>
+        )}
+      </div>
 
-      {nextPageNumber && (
-        <Link to={(location) => addQueryParamToCurrentPath(location, "page", nextPageNumber)}>
-          Next &#62;&#62;
-        </Link>
-      )}
-    </p>
+      <div className="paginator-item current-page">
+        <strong>Page {currentPageNumber}</strong>
+      </div>
+
+      <div className="paginator-item next-page">
+        {nextPageNumber && (
+          <Link to={(location) => addQueryParamToCurrentPath(location, "page", nextPageNumber)}>
+            Next »
+          </Link>
+        )}
+      </div>
+
+      <div className="paginator-item last-page">
+        {nextPageNumber && nextPageNumber !== totalPages && (
+          <Link to={(location) => addQueryParamToCurrentPath(location, "page", totalPages)}>
+            Last »
+          </Link>
+        )}
+      </div>
+    </div>
   );
 };
